0d8fad14482cdc5224c58e97df2e3f4d89c6a4b2
Bunnichi <Treemexican123@gmail.com>
2024-10-08 14:01:52 -0500

    removing redudent code

--- src/main/java/net/rabbichi/TACZWeaponAssemblyParts/TACZWeaponAssemblyParts.java
+++ src/main/java/net/rabbichi/TACZWeaponAssemblyParts/TACZWeaponAssemblyParts.java
@@ -16,9 +16,7 @@ import org.slf4j.Logger;
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(TACZWeaponAssemblyParts.MOD_ID)
 public class TACZWeaponAssemblyParts {
-    // Define mod id in a common place for everything to reference
     public static final String MOD_ID = "taczweaponsssemblyparts";
-    // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
 
